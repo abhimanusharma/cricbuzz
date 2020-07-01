ca118ce3bb4023e3432edb5baa8ec04a8698fde0
Abhimanu Sharma <dragonabv1@gmail.com>
2020-07-01 21:11:08 +0530

    Added relation to match with team

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Match;
 use Illuminate\Http\Request;
 
 class MatchesController extends Controller
 {
     public function index()
     {
-
+        return response()->json(Match::with('team1')->with('team2')->get());
     }
 
     public function show()
--- app/Match.php
+++ app/Match.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Match extends Model
 {
-    //
+    public function team1()
+    {
+        return $this->hasOne('App\Team', 'id', 'team1_id');
+    }
+    public function team2()
+    {
+        return $this->hasOne('App\Team', 'id', 'team2_id');
+    }
 }
